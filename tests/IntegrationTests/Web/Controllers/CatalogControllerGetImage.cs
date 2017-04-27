ac89e73bf465fbcdbc3460d6b5de9bf20f76f8ff
Steve Smith <steve@kentsmiths.com>
2017-04-27 16:53:51 -0400

    getting initial tests working

---
+++ tests/IntegrationTests/Web/Controllers/CatalogControllerGetImage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.eShopWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntegrationTests.Web.Controllers
+{
+    public class CatalogControllerGetImage
+    {
+        [Fact]
+        public void ReturnsFileContentResultGivenValidId()
+        {
+            //var controller = new CatalogController()
+        }
+    }
+}
