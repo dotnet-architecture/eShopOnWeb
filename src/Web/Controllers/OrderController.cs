6ea8fc9fbfd52392876cc51ea2f6c0fca0315193
Steve Smith <steve@kentsmiths.com>
2017-09-13 18:48:31 -0400

    WIP

---
+++ src/Web/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.eShopWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Microsoft.eShopWeb.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+
+        public OrderController() { }
+
+        public async Task<IActionResult> Detail(string orderId)
+        {
+            //var user = _appUserParser.Parse(HttpContext.User);
+
+            //var order = await _orderSvc.GetOrder(user, orderId);
+            return View();
+        }
+
+        //public async Task<IActionResult> Index(Order item)
+        public async Task<IActionResult> Index()
+        {
+            //var user = _appUserParser.Parse(HttpContext.User);
+            //var vm = await _orderSvc.GetMyOrders(user);
+            return View();
+        }
+    }
+}
