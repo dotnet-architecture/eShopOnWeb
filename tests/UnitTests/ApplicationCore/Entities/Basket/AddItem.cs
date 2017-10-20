0a76ffb2ce654fa36153f9680d01d018b8d0f399
Steve Smith <steve@kentsmiths.com>
2017-10-20 11:15:14 -0400

    Adding tests for AddItem

---
+++ tests/UnitTests/ApplicationCore/Entities/Basket/AddItem.cs
@@ -0,0 +1,59 @@
+﻿using ApplicationCore.Specifications;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests
+{
+    public class AddItem
+    {
+        private int _testCatalogItemId = 123;
+        private decimal _testUnitPrice = 1.23m;
+        private int _testQuantity = 2;
+
+        [Fact]
+        public void AddsBasketItemIfNotPresent()
+        {
+            var basket = new Basket();
+            basket.AddItem(_testCatalogItemId, _testUnitPrice, _testQuantity);
+
+            var firstItem = basket.Items.Single();
+            Assert.Equal(_testCatalogItemId, firstItem.CatalogItemId);
+            Assert.Equal(_testUnitPrice, firstItem.UnitPrice);
+            Assert.Equal(_testQuantity, firstItem.Quantity);
+        }
+
+        [Fact]
+        public void IncrementsQuantityOfItemIfPresent()
+        {
+            var basket = new Basket();
+            basket.AddItem(_testCatalogItemId, _testUnitPrice, _testQuantity);
+            basket.AddItem(_testCatalogItemId, _testUnitPrice, _testQuantity);
+
+            var firstItem = basket.Items.Single();
+            Assert.Equal(_testQuantity * 2, firstItem.Quantity);
+        }
+
+        [Fact]
+        public void KeepsOriginalUnitPriceIfMoreItemsAdded()
+        {
+            var basket = new Basket();
+            basket.AddItem(_testCatalogItemId, _testUnitPrice, _testQuantity);
+            basket.AddItem(_testCatalogItemId, _testUnitPrice * 2, _testQuantity);
+
+            var firstItem = basket.Items.Single();
+            Assert.Equal(_testUnitPrice, firstItem.UnitPrice);
+        }
+
+        [Fact]
+        public void DefaultsToQuantityOfOne()
+        {
+            var basket = new Basket();
+            basket.AddItem(_testCatalogItemId, _testUnitPrice);
+
+            var firstItem = basket.Items.Single();
+            Assert.Equal(1, firstItem.Quantity);
+        }
+    }
+}
