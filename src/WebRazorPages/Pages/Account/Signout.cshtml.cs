4cb50daf1f967aa617bf9a3c1d5d91cec0eba2ba
Steve Smith <steve@kentsmiths.com>
2017-10-23 09:07:10 -0400

    Working on auth

---
+++ src/WebRazorPages/Pages/Account/Signout.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.eShopWeb.RazorPages.Pages.Account
+{
+    public class SignoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SignoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToPage("/Index");
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
