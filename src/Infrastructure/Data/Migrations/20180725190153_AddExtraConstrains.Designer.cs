77540caf2968dc35e1b6446c6feb61f00cda7c46
Matthias Beerens <beerens.matthias@hotmail.com>
2018-07-25 21:07:04 +0200

    Add max length and required constraints

--- src/Infrastructure/Data/Migrations/20180725190153_AddExtraConstrains.Designer.cs
+++ src/Infrastructure/Data/Migrations/20180725190153_AddExtraConstrains.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.eShopWeb.Infrastructure.Data;
 
 namespace Microsoft.eShopWeb.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class CatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20180725190153_AddExtraConstrains")]
+    partial class AddExtraConstrains
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
