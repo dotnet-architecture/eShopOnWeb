dc318d98db3869c03fd6fe9b4336e4f13e7f1122
rafsanulhasan <rafsanulhasan@outlook.com>
2018-09-05 18:20:15 +0600

    Specified default RequireHttpsPermanent and SslPort for RequiredHtts Attribute mon the MVC pipeline Add HttpsRedirection service to the DI container and specified HttpsPort

--- src/WebRazorPages/Startup.cs
+++ src/WebRazorPages/Startup.cs
@@ -1,14 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Services;
 using Microsoft.eShopWeb.Infrastructure.Data;
 using Microsoft.eShopWeb.Infrastructure.Identity;
 using Microsoft.eShopWeb.Infrastructure.Logging;
 using Microsoft.eShopWeb.Infrastructure.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.RazorPages.Interfaces;
 using Microsoft.eShopWeb.RazorPages.Services;
 using Microsoft.Extensions.Configuration;
@@ -18,148 +18,158 @@ using System.Text;
 
 namespace Microsoft.eShopWeb.RazorPages
 {
-    public class Startup
-    {
-        private IServiceCollection _services;
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		private IServiceCollection _services;
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        public void ConfigureDevelopmentServices(IServiceCollection services)
-        {
-            // use in-memory database
-            ConfigureTestingServices(services);
+		public void ConfigureDevelopmentServices(IServiceCollection services)
+		{
+			// use in-memory database
+			ConfigureTestingServices(services);
 
-            // use real database
-            // ConfigureProductionServices(services);
+			// use real database
+			// ConfigureProductionServices(services);
 
-        }
-        public void ConfigureTestingServices(IServiceCollection services)
-        {
-            // use in-memory database
-            services.AddDbContext<CatalogContext>(c =>
-                c.UseInMemoryDatabase("Catalog"));
+		}
+		public void ConfigureTestingServices(IServiceCollection services)
+		{
+			// use in-memory database
+			services.AddDbContext<CatalogContext>(c =>
+			    c.UseInMemoryDatabase("Catalog"));
 
-            // Add Identity DbContext
-            services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseInMemoryDatabase("Identity"));
+			// Add Identity DbContext
+			services.AddDbContext<AppIdentityDbContext>(options =>
+			    options.UseInMemoryDatabase("Identity"));
 
-            ConfigureServices(services);
-        }
+			ConfigureServices(services);
+		}
 
-        public void ConfigureProductionServices(IServiceCollection services)
-        {
-            // use real database
-            services.AddDbContext<CatalogContext>(c =>
-            {
-                try
-                {
-                    // Requires LocalDB which can be installed with SQL Server Express 2016
-                    // https://www.microsoft.com/en-us/download/details.aspx?id=54284
-                    c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection"));
-                }
-                catch (System.Exception ex)
-                {
-                    var message = ex.Message;
-                }
-            });
+		public void ConfigureProductionServices(IServiceCollection services)
+		{
+			// use real database
+			services.AddDbContext<CatalogContext>(c =>
+			{
+				try
+				{
+					// Requires LocalDB which can be installed with SQL Server Express 2016
+					// https://www.microsoft.com/en-us/download/details.aspx?id=54284
+					c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection"));
+				}
+				catch (System.Exception ex)
+				{
+					string message = ex.Message;
+				}
+			});
 
-            // Add Identity DbContext
-            services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+			// Add Identity DbContext
+			services.AddDbContext<AppIdentityDbContext>(options =>
+			    options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
 
-            ConfigureServices(services);
-        }
+			ConfigureServices(services);
+		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppIdentityDbContext>()
-                .AddDefaultTokenProviders();
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+			    .AddEntityFrameworkStores<AppIdentityDbContext>()
+			    .AddDefaultTokenProviders();
 
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromHours(1);
-                options.LoginPath = "/Account/Signin";
-                options.LogoutPath = "/Account/Signout";
-            });
+			services.ConfigureApplicationCookie(options =>
+			{
+				options.Cookie.HttpOnly = true;
+				options.ExpireTimeSpan = TimeSpan.FromHours(1);
+				options.LoginPath = "/Account/Signin";
+				options.LogoutPath = "/Account/Signout";
+			});
 
-            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-            services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+			services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+			services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
 
-            services.AddScoped<ICatalogService, CachedCatalogService>();
-            services.AddScoped<IBasketService, BasketService>();
-            services.AddScoped<IBasketViewModelService, BasketViewModelService>();
-            services.AddScoped<IOrderService, OrderService>();
-            services.AddScoped<IOrderRepository, OrderRepository>();
-            services.AddScoped<CatalogService>();
-            services.Configure<CatalogSettings>(Configuration);
-            services.AddSingleton<IUriComposer>(new UriComposer(Configuration.Get<CatalogSettings>()));
+			services.AddScoped<ICatalogService, CachedCatalogService>();
+			services.AddScoped<IBasketService, BasketService>();
+			services.AddScoped<IBasketViewModelService, BasketViewModelService>();
+			services.AddScoped<IOrderService, OrderService>();
+			services.AddScoped<IOrderRepository, OrderRepository>();
+			services.AddScoped<CatalogService>();
+			services.Configure<CatalogSettings>(Configuration);
+			services.AddSingleton<IUriComposer>(new UriComposer(Configuration.Get<CatalogSettings>()));
 
-            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
-            services.AddTransient<IEmailSender, EmailSender>();
+			services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+			services.AddTransient<IEmailSender, EmailSender>();
 
-            // Add memory cache services
-            services.AddMemoryCache();
+			// Add memory cache services
+			services.AddMemoryCache();
 
-            services.AddMvc()
-                .SetCompatibilityVersion(AspNetCore.Mvc.CompatibilityVersion.Version_2_1)
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AuthorizeFolder("/Order");
-                    options.Conventions.AuthorizePage("/Basket/Checkout");
-                });
+			services
+				.AddMvc(opts=>
+				{
+					opts.RequireHttpsPermanent = true;
+					opts.SslPort = 44389;
+				})
+				.SetCompatibilityVersion(AspNetCore.Mvc.CompatibilityVersion.Version_2_1)
+				.AddRazorPagesOptions(options =>
+				{
+					options.Conventions.AuthorizeFolder("/Order");
+					options.Conventions.AuthorizePage("/Basket/Checkout");
+				});
 
-            _services = services;
-        }
+			services.AddHttpsRedirection(opts =>
+			{
+				opts.HttpsPort = 44389;
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app,
-            IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                ListAllRegisteredServices(app);
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Catalog/Error");
-                app.UseHsts();
-            }
+			_services = services;
+		}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseAuthentication();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app,
+		    IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				ListAllRegisteredServices(app);
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Catalog/Error");
+				app.UseHsts();
+			}
 
-            app.UseMvc();
-        }
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseAuthentication();
 
-        private void ListAllRegisteredServices(IApplicationBuilder app)
-        {
-            app.Map("/allservices", builder => builder.Run(async context =>
-            {
-                var sb = new StringBuilder();
-                sb.Append("<h1>All Services</h1>");
-                sb.Append("<table><thead>");
-                sb.Append("<tr><th>Type</th><th>Lifetime</th><th>Instance</th></tr>");
-                sb.Append("</thead><tbody>");
-                foreach (var svc in _services)
-                {
-                    sb.Append("<tr>");
-                    sb.Append($"<td>{svc.ServiceType.FullName}</td>");
-                    sb.Append($"<td>{svc.Lifetime}</td>");
-                    sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
-                    sb.Append("</tr>");
-                }
-                sb.Append("</tbody></table>");
-                await context.Response.WriteAsync(sb.ToString());
-            }));
-        }
-    }
+			app.UseMvc();
+		}
+
+		private void ListAllRegisteredServices(IApplicationBuilder app)
+		{
+			app.Map("/allservices", builder => builder.Run(async context =>
+			{
+				StringBuilder sb = new StringBuilder();
+				sb.Append("<h1>All Services</h1>");
+				sb.Append("<table><thead>");
+				sb.Append("<tr><th>Type</th><th>Lifetime</th><th>Instance</th></tr>");
+				sb.Append("</thead><tbody>");
+				foreach (ServiceDescriptor svc in _services)
+				{
+					sb.Append("<tr>");
+					sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+					sb.Append($"<td>{svc.Lifetime}</td>");
+					sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+					sb.Append("</tr>");
+				}
+				sb.Append("</tbody></table>");
+				await context.Response.WriteAsync(sb.ToString());
+			}));
+		}
+	}
 }
