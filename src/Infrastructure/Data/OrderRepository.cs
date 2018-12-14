e3f7bacedf67970f505fdbcf4df1079b75a1d3aa
Eric Fleming <eric-fleming18@hotmail.com>
2018-12-14 18:38:36 -0500

    Removing unused usings
    - also removed the sync method as it is not used anywhere in the solution. The Async should be the preferred one.

--- src/Infrastructure/Data/OrderRepository.cs
+++ src/Infrastructure/Data/OrderRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Infrastructure.Data
@@ -11,15 +10,7 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         public OrderRepository(CatalogContext dbContext) : base(dbContext)
         {
         }
-
-        public Order GetByIdWithItems(int id)
-        {
-            return _dbContext.Orders
-                .Include(o => o.OrderItems)
-                .Include($"{nameof(Order.OrderItems)}.{nameof(OrderItem.ItemOrdered)}")
-                .FirstOrDefault(x => x.Id == id);
-        }
-
+        
         public Task<Order> GetByIdWithItemsAsync(int id)
         {
             return _dbContext.Orders
