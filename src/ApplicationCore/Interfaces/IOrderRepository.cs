c85726bf44cadfaa908977e6e907614bfda42b43
Eric Fleming <eric-fleming18@hotmail.com>
2018-12-14 20:54:23 -0500

    Fix 147 include order id in details query (#169)
    * Adding Id to query so the correct order details are always pulled back
    
    * Removing unused usings
    
    - also removed the sync method as it is not used anywhere in the solution. The Async should be the preferred one.
    
    * Adding integration test for GetByIdWithItemAsync
    
    * Rename test

--- src/ApplicationCore/Interfaces/IOrderRepository.cs
+++ src/ApplicationCore/Interfaces/IOrderRepository.cs
@@ -4,9 +4,8 @@ using System.Threading.Tasks;
 namespace Microsoft.eShopWeb.ApplicationCore.Interfaces
 {
 
-    public interface IOrderRepository : IRepository<Order>, IAsyncRepository<Order>
+    public interface IOrderRepository : IAsyncRepository<Order>
     {
-        Order GetByIdWithItems(int id);
         Task<Order> GetByIdWithItemsAsync(int id);
     }
 }
--- src/Infrastructure/Data/OrderRepository.cs
+++ src/Infrastructure/Data/OrderRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Infrastructure.Data
@@ -11,21 +10,13 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         public OrderRepository(CatalogContext dbContext) : base(dbContext)
         {
         }
-
-        public Order GetByIdWithItems(int id)
-        {
-            return _dbContext.Orders
-                .Include(o => o.OrderItems)
-                .Include($"{nameof(Order.OrderItems)}.{nameof(OrderItem.ItemOrdered)}")
-                .FirstOrDefault();
-        }
-
+        
         public Task<Order> GetByIdWithItemsAsync(int id)
         {
             return _dbContext.Orders
                 .Include(o => o.OrderItems)
                 .Include($"{nameof(Order.OrderItems)}.{nameof(OrderItem.ItemOrdered)}")
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
