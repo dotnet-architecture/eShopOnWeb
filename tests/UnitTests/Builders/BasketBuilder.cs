eb027508413f8f9ebdbe65974da49dd1598dc4e7
Eric Fleming <eric-fleming18@hotmail.com>
2018-12-28 10:40:35 -0500

    Remove basket items when basket is deleted (#170)
    * Proving out removing the basket items at the same time the basket is deleted
    
    * Fixing existing unit tests
    
    * Adding a unit test for the Deletebasket method
    
    * Rename test and deleting basket items before basket
    
    * Added integration test for DeleteAsync method for BasketItems
    
    - Also added a BasketBuilder to create a Basket with no items, or one item.

---
+++ tests/UnitTests/Builders/BasketBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+
+namespace Microsoft.eShopWeb.UnitTests.Builders
+{
+    public class BasketBuilder
+    {
+        private Basket _basket;
+        public string BasketBuyerId => "testbuyerId@test.com";
+        public int BasketId => 1;
+
+        public BasketBuilder()
+        {
+            _basket = WithNoItems();
+        }
+
+        public Basket Build()
+        {
+            return _basket;
+        }
+
+        public Basket WithNoItems()
+        {
+            _basket = new Basket { BuyerId = BasketBuyerId, Id = BasketId };
+            return _basket;
+        }
+
+        public Basket WithOneBasketItem()
+        {
+            _basket = new Basket { BuyerId = BasketBuyerId, Id = BasketId };
+            _basket.AddItem(2, 3.40m, 4);
+            return _basket;
+        }
+    }
+}
