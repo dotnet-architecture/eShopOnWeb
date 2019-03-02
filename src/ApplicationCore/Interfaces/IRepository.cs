b084d4e770c3632193193994536e35ca5d9ab751
Eric Fleming <eric-fleming18@hotmail.com>
2019-03-01 22:07:46 -0500

    Removing List and all the places it was used

--- src/ApplicationCore/Interfaces/IRepository.cs
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -6,7 +6,6 @@ namespace Microsoft.eShopWeb.ApplicationCore.Interfaces
     public interface IRepository<T> where T : BaseEntity
     {
         T GetSingleBySpec(ISpecification<T> spec);
-        IEnumerable<T> List(ISpecification<T> spec);
         T Add(T entity);
         void Update(T entity);
         void Delete(T entity);
