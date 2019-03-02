0e94161dbed652597ddacdc23286b6c7d8c3e87b
Eric Fleming <eric-fleming18@hotmail.com>
2019-03-01 22:10:32 -0500

    Removing update

--- src/ApplicationCore/Interfaces/IRepository.cs
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -6,7 +6,6 @@ namespace Microsoft.eShopWeb.ApplicationCore.Interfaces
     public interface IRepository<T> where T : BaseEntity
     {
         T GetSingleBySpec(ISpecification<T> spec);
-        void Update(T entity);
         void Delete(T entity);
         int Count(ISpecification<T> spec);
     }
