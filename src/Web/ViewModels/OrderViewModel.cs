88e8ec33459fdb5ee0df5d6061b8f4a9acf753d9
Eric Fleming <eric-fleming18@hotmail.com>
2019-05-23 19:30:04 -0400

    Using constant

--- src/Web/ViewModels/OrderViewModel.cs
+++ src/Web/ViewModels/OrderViewModel.cs
@@ -6,10 +6,12 @@ namespace Microsoft.eShopWeb.Web.ViewModels
 {
     public class OrderViewModel
     {
+        private const string DEFAULT_STATUS = "Pending";
+
         public int OrderNumber { get; set; }
         public DateTimeOffset OrderDate { get; set; }
         public decimal Total { get; set; }
-        public string Status => "Pending";
+        public string Status => DEFAULT_STATUS;
         public Address ShippingAddress { get; set; }
         public List<OrderItemViewModel> OrderItems { get; set; } = new List<OrderItemViewModel>();
     }
