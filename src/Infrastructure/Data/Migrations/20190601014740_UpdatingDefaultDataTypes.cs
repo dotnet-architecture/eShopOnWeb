8924df0e1b45e6b503f735d89722e8a2f47a2326
Eric Fleming <eric-fleming18@hotmail.com>
2019-05-31 21:48:25 -0400

    Adding migration for updating defaults

---
+++ src/Infrastructure/Data/Migrations/20190601014740_UpdatingDefaultDataTypes.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data.Migrations
+{
+    public partial class UpdatingDefaultDataTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketItem_Baskets_BasketId",
+                table: "BasketItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketItem",
+                table: "BasketItem");
+
+            migrationBuilder.RenameTable(
+                name: "BasketItem",
+                newName: "BasketItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BasketItem_BasketId",
+                table: "BasketItems",
+                newName: "IX_BasketItems_BasketId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketItems_Baskets_BasketId",
+                table: "BasketItems",
+                column: "BasketId",
+                principalTable: "Baskets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketItems_Baskets_BasketId",
+                table: "BasketItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems");
+
+            migrationBuilder.RenameTable(
+                name: "BasketItems",
+                newName: "BasketItem");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BasketItems_BasketId",
+                table: "BasketItem",
+                newName: "IX_BasketItem_BasketId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketItem",
+                table: "BasketItem",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketItem_Baskets_BasketId",
+                table: "BasketItem",
+                column: "BasketId",
+                principalTable: "Baskets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
