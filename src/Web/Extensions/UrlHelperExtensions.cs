2fe08969c97296e7b4b5e5c4f325d8984a346ad3
Eric Fleming <eric-fleming18@hotmail.com>
2019-06-21 21:18:50 -0400

    Removing unused UrlHelperExtension

--- src/Web/Extensions/UrlHelperExtensions.cs
+++ src/Web/Extensions/UrlHelperExtensions.cs
@@ -12,14 +12,5 @@ namespace Microsoft.AspNetCore.Mvc
                 values: new { userId, code },
                 protocol: scheme);
         }
-
-        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-        {
-            return urlHelper.Action(
-                action: nameof(AccountController.ResetPassword),
-                controller: "Account",
-                values: new { userId, code },
-                protocol: scheme);
-        }
     }
 }
