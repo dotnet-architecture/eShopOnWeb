efc7e90041d267115f877689f26815980a0154e3
Eric Fleming <eric-fleming18@hotmail.com>
2019-06-21 21:39:52 -0400

    Adding confirmEmail scaffolding
    - Also updating the UrlHelpExtensions to not have a dependency on the AccountController

--- src/Web/Extensions/UrlHelperExtensions.cs
+++ src/Web/Extensions/UrlHelperExtensions.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.eShopWeb.Web.Controllers;
-
-namespace Microsoft.AspNetCore.Mvc
+﻿namespace Microsoft.AspNetCore.Mvc
 {
     public static class UrlHelperExtensions
     {
         public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
         {
             return urlHelper.Action(
-                action: nameof(AccountController.ConfirmEmail),
-                controller: "Account",
+                action: "GET",
+                controller: "ConfirmEmail",
                 values: new { userId, code },
                 protocol: scheme);
         }
