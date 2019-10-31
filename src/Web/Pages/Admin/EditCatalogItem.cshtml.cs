ce32209d7f7c8ec4e529800e43dcde332a97185f
Eric Fleming <eric-fleming18@hotmail.com>
2019-10-31 16:13:14 -0400

    Allow user to edit a catalog item

--- src/Web/Pages/Admin/EditCatalogItem.cshtml.cs
+++ src/Web/Pages/Admin/EditCatalogItem.cshtml.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.eShopWeb.ApplicationCore.Constants;
-using Microsoft.eShopWeb.ApplicationCore.Entities;
-using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Web.Interfaces;
 using Microsoft.eShopWeb.Web.ViewModels;
 using System.Threading.Tasks;
 
@@ -11,25 +11,29 @@ namespace Microsoft.eShopWeb.Web.Pages.Admin
     [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
     public class EditCatalogItemModel : PageModel
     {
-        private readonly IAsyncRepository<CatalogItem> _catalogItemRepository;
+        private readonly ICatalogItemViewModelService _catalogItemViewModelService;
 
-        public EditCatalogItemModel(IAsyncRepository<CatalogItem> catalogItemRepository)
+        public EditCatalogItemModel(ICatalogItemViewModelService catalogItemViewModelService)
         {
-            _catalogItemRepository = catalogItemRepository;
+            _catalogItemViewModelService = catalogItemViewModelService;
         }
 
+        [BindProperty]
         public CatalogItemViewModel CatalogModel { get; set; } = new CatalogItemViewModel();
 
         public async Task OnGet(CatalogItemViewModel catalogModel)
         {
             CatalogModel = catalogModel;
+        }
 
-            var catalogItem = await _catalogItemRepository.GetByIdAsync(catalogModel.Id);
-            catalogItem.Name = "New Name";
-            
-            await _catalogItemRepository.UpdateAsync(catalogItem);
-            
-            var catalogITem = await _catalogItemRepository.GetByIdAsync(catalogModel.Id);
+        public async Task<IActionResult> OnPostAsync(CatalogItemViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _catalogItemViewModelService.UpdateCatalogItem(CatalogModel);
+            }
+
+            return RedirectToPage("/Admin/Index");
         }
     }
 }
---
+++ src/Web/Services/CatalogItemViewModelService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Web.Interfaces;
+using Microsoft.eShopWeb.Web.ViewModels;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.Web.Services
+{
+    public class CatalogItemViewModelService : ICatalogItemViewModelService
+    {
+        private readonly IAsyncRepository<CatalogItem> _catalogItemRepository;
+
+        public CatalogItemViewModelService(IAsyncRepository<CatalogItem> catalogItemRepository)
+        {
+            _catalogItemRepository = catalogItemRepository;
+        }
+
+        public async Task UpdateCatalogItem(CatalogItemViewModel viewModel)
+        {
+            //Get existing CatalogItem
+            var existingCatalogItem = await _catalogItemRepository.GetByIdAsync(viewModel.Id);
+
+            //Build updated CatalogItem
+            var updatedCatalogItem = existingCatalogItem;
+            updatedCatalogItem.Name = viewModel.Name;
+            updatedCatalogItem.Price = viewModel.Price;
+
+            await _catalogItemRepository.UpdateAsync(updatedCatalogItem);
+        }
+    }
+}
