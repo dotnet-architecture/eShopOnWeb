0bd3bd92cdfde8eaf088c55317b861c7072a25e9
Eric Fleming <eric-fleming18@hotmail.com>
2019-12-10 21:41:29 -0500

    Creating Cache helpers with unit tests

--- src/Web/Pages/Admin/Index.cshtml.cs
+++ src/Web/Pages/Admin/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.eShopWeb.ApplicationCore.Constants;
+using Microsoft.eShopWeb.Web.Extensions;
 using Microsoft.eShopWeb.Web.Services;
 using Microsoft.eShopWeb.Web.ViewModels;
 using Microsoft.Extensions.Caching.Memory;
@@ -12,7 +13,6 @@ namespace Microsoft.eShopWeb.Web.Pages.Admin
     public class IndexModel : PageModel
     {
         private readonly ICatalogViewModelService _catalogViewModelService;
-        private readonly string _itemsKeyTemplate = "items-{0}-{1}-{2}-{3}";
         private readonly IMemoryCache _cache;
 
         public IndexModel(ICatalogViewModelService catalogViewModelService, IMemoryCache cache)
@@ -25,15 +25,11 @@ namespace Microsoft.eShopWeb.Web.Pages.Admin
 
         public async Task OnGet(CatalogIndexViewModel catalogModel, int? pageId)
         {
-            var pageIndex = pageId ?? 0;
-            var itemsPerPage = Constants.ITEMS_PER_PAGE;
-            var brandFilterApplied = catalogModel.BrandFilterApplied;
-            var typesFilterApplied = catalogModel.TypesFilterApplied;
-            var cacheKey = string.Format(_itemsKeyTemplate, pageIndex, itemsPerPage, brandFilterApplied, typesFilterApplied);
+            var cacheKey = CacheHelpers.GenerateCatalogItemCacheKey(pageId.GetValueOrDefault(), Constants.ITEMS_PER_PAGE, catalogModel.BrandFilterApplied, catalogModel.TypesFilterApplied);
 
             _cache.Remove(cacheKey);
 
-            CatalogModel = await _catalogViewModelService.GetCatalogItems(pageIndex, itemsPerPage, brandFilterApplied, typesFilterApplied);
+            CatalogModel = await _catalogViewModelService.GetCatalogItems(pageId.GetValueOrDefault(), Constants.ITEMS_PER_PAGE, catalogModel.BrandFilterApplied, catalogModel.TypesFilterApplied);
         }
     }
 }
---
+++ tests/UnitTests/Web/Extensions/CacheHelpersTests/GenerateCatalogItemCacheKey_Should.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.eShopWeb.Web;
+using Microsoft.eShopWeb.Web.Extensions;
+using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.Web.Extensions.CacheHelpersTests
+{
+    public class GenerateCatalogItemCacheKey_Should
+    {
+        [Fact]
+        public void ReturnCatalogItemCacheKey()
+        {
+            var pageIndex = 0;
+            int? brandId = null;
+            int? typeId = null;
+
+            var result = CacheHelpers.GenerateCatalogItemCacheKey(pageIndex, Constants.ITEMS_PER_PAGE, brandId, typeId);
+
+            Assert.Equal("items-0-10--", result);
+        }
+    }
+}
