92ca22cf8b0ed27ab78ce1f2662450e8e145db7b
Cédric Michel <cedric.michel@outlook.be>
2020-05-13 19:45:52 +0200

    Feature load improvement (#374)
    * use lambda expression  => improve database call
    
    * use lambda instead  for each replace call to repository to reduce call to database
    
    * improve readability and maintainability, and add order by
    
    * clean semicolon
    
    * fix use correct catalog item id

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
-using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
-using System.Threading.Tasks;
+﻿using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
-using System.Collections.Generic;
-using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services
 {
@@ -33,14 +33,18 @@ namespace Microsoft.eShopWeb.ApplicationCore.Services
             var basket = await _basketRepository.FirstOrDefaultAsync(basketSpec);
 
             Guard.Against.NullBasket(basketId, basket);
-            var items = new List<OrderItem>();
-            foreach (var item in basket.Items)
+
+            var catalogItemsSpecification = new CatalogItemsSpecification(basket.Items.Select(item => item.CatalogItemId).ToArray());
+            var catalogItems = await _itemRepository.ListAsync(catalogItemsSpecification);
+
+            var items = basket.Items.Select(basketItem =>
             {
-                var catalogItem = await _itemRepository.GetByIdAsync(item.CatalogItemId);
-                var itemOrdered = new CatalogItemOrdered(catalogItem.Id, catalogItem.Name,_uriComposer.ComposePicUri(catalogItem.PictureUri));
-                var orderItem = new OrderItem(itemOrdered, item.UnitPrice, item.Quantity);
-                items.Add(orderItem);
-            }
+                var catalogItem = catalogItems.First(c => c.Id == basketItem.CatalogItemId);
+                var itemOrdered = new CatalogItemOrdered(catalogItem.Id, catalogItem.Name, _uriComposer.ComposePicUri(catalogItem.PictureUri));
+                var orderItem = new OrderItem(itemOrdered, basketItem.UnitPrice, basketItem.Quantity);
+                return orderItem;
+            }).ToList();
+
             var order = new Order(basket.BuyerId, shippingAddress, items);
 
             await _orderRepository.AddAsync(order);
