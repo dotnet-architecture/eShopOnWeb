c5d1f2baf3dfde96cba58d1e995242123888cf93
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-11 10:38:03 +0000

    Update Program to seed the DB

--- src/eShopOnBlazorWasm/Source/Server/Program.cs
+++ src/eShopOnBlazorWasm/Source/Server/Program.cs
@@ -1,9 +1,11 @@
 namespace eShopOnBlazorWasm.Server
 {
   using Microsoft.AspNetCore.Hosting;
-
-
+  using Microsoft.eShopWeb.Infrastructure.Data;
+  using Microsoft.Extensions.DependencyInjection;
   using Microsoft.Extensions.Hosting;
+  using Microsoft.Extensions.Logging;
+  using System;
 
   public class Program
   {
@@ -19,6 +21,31 @@ namespace eShopOnBlazorWasm.Server
           }
         );
 
-    public static void Main(string[] aArgumentArray) => CreateHostBuilder(aArgumentArray).Build().Run();
+    public static async System.Threading.Tasks.Task Main(string[] aArgumentArray)
+    {
+      IHost host = CreateHostBuilder(aArgumentArray).Build();
+
+      using (IServiceScope serviceScope = host.Services.CreateScope())
+      {
+        IServiceProvider serviceProvider = serviceScope.ServiceProvider;
+        ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        try
+        {
+          CatalogContext catalogContext = serviceProvider.GetRequiredService<CatalogContext>();
+          await CatalogContextSeed.SeedAsync(catalogContext, loggerFactory);
+
+          //var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+          //var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+          //await AppIdentityDbContextSeed.SeedAsync(userManager, roleManager);
+        }
+        catch (Exception ex)
+        {
+          ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+          logger.LogError(ex, "An error occurred seeding the DB.");
+        }
+      }
+
+      host.Run();
+    }
   }
 }
