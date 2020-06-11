031de0cbc5faef6af6c1bda1f8feeabcf321c09c
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-11 10:15:59 +0000

    Use Repository and Automapper to build and return the Response
    Tests need updated to validate.

--- src/eShopOnBlazorWasm/Source/Server/Features/Catalog/CatalogType/GetCatalogTypes/GetCatalogTypesHandler.cs
+++ src/eShopOnBlazorWasm/Source/Server/Features/Catalog/CatalogType/GetCatalogTypes/GetCatalogTypesHandler.cs
@@ -1,6 +1,9 @@
 namespace eShopOnBlazorWasm.Features.Catalogs
 {
+  using AutoMapper;
   using MediatR;
+  using Microsoft.eShopWeb.ApplicationCore.Entities;
+  using Microsoft.eShopWeb.ApplicationCore.Interfaces;
   using System;
   using System.Collections.Generic;
   using System.Linq;
@@ -9,15 +12,27 @@ namespace eShopOnBlazorWasm.Features.Catalogs
   
   public class GetCatalogTypesHandler : IRequestHandler<GetCatalogTypesRequest, GetCatalogTypesResponse>
   {
+    public GetCatalogTypesHandler(IAsyncRepository<CatalogType> aCatalogTypeRepository, IMapper aMapper)
+    {
+      CatalogTypeRepository = aCatalogTypeRepository;
+      Mapper = aMapper;
+    }
+
+    public IAsyncRepository<CatalogType> CatalogTypeRepository { get; }
+    public IMapper Mapper { get; }
+
     public async Task<GetCatalogTypesResponse> Handle
     (
       GetCatalogTypesRequest aGetCatalogTypesRequest,
       CancellationToken aCancellationToken
     )
     {
+      IReadOnlyList<CatalogType> catalogTypes = await CatalogTypeRepository.ListAllAsync();
+      
       var response = new GetCatalogTypesResponse(aGetCatalogTypesRequest.Id);
+      response.CatalogTypes.AddRange(catalogTypes.Select(Mapper.Map<CatalogTypeDto>));
 
-      return await Task.Run(() => response);
+      return response;
     }
   }
 }
