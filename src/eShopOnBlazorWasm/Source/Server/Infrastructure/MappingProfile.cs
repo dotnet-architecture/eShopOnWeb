0ad29c2b9bcb4fb38c9cac4a549a6c0a9a38a10e
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-11 11:20:07 +0000

    Add DTO Mapping profile

--- src/eShopOnBlazorWasm/Source/Server/Infrastructure/MappingProfile.cs
+++ src/eShopOnBlazorWasm/Source/Server/Infrastructure/MappingProfile.cs
@@ -1,13 +1,16 @@
 ﻿namespace eShopOnBlazorWasm.Infrastructure
 {
   using AutoMapper;
+  using eShopOnBlazorWasm.Features.CatalogBrand;
   using eShopOnBlazorWasm.Features.Catalogs;
   using Microsoft.eShopWeb.ApplicationCore.Entities;
+
   public class MappingProfile : Profile
   {
     public MappingProfile()
     {
       CreateMap<CatalogType, CatalogTypeDto>();
+      CreateMap<CatalogBrand, CatalogBrandDto>();
     }
   }
 }
