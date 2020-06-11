dab37264355a2d9190e7d0f7fb78040bae824d28
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-11 11:18:54 +0000

    Add Dto for CatalogBrand

---
+++ src/eShopOnBlazorWasm/Source/Api/Features/CatalogBrand/CatalogBrandDto.cs
@@ -0,0 +1,18 @@
+﻿namespace eShopOnBlazorWasm.Features.CatalogBrand
+{
+  public class CatalogBrandDto
+  {
+    /// <summary>
+    /// The Brand Name
+    /// </summary>
+    /// <example>Azure</example>
+    public string Brand { get; set; }
+
+    public CatalogBrandDto() { }
+
+    public CatalogBrandDto(string aBrand) : this()
+    {
+      Brand = aBrand;
+    }
+  }
+}
