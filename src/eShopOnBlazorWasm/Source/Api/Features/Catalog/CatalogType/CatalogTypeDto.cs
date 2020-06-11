630e00f7402b05ae32c69baffd45ba2b1201aaa7
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-11 10:06:30 +0000

    We are going to be transferring a collection of items so we should have a DTO

---
+++ src/eShopOnBlazorWasm/Source/Api/Features/Catalog/CatalogType/CatalogTypeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace eShopOnBlazorWasm.Features.Catalogs
+{
+  public class CatalogTypeDto
+  {
+    /// <summary>
+    /// Catalog Type
+    /// </summary>
+    /// <example> </example>
+    public string Type { get; set; }
+
+    public CatalogTypeDto() { }
+    public CatalogTypeDto(string aType) : this()
+    {
+      Type = aType;
+    }
+  }
+}
