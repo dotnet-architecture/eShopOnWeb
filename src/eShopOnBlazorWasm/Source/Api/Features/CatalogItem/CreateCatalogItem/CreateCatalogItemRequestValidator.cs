4da4c3a9cd04c36f3cc0082811d5618fcd07a1f1
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-12 07:16:21 +0000

    Add Validator and test

---
+++ src/eShopOnBlazorWasm/Source/Api/Features/CatalogItem/CreateCatalogItem/CreateCatalogItemRequestValidator.cs
@@ -0,0 +1,41 @@
+﻿namespace eShopOnBlazorWasm.Features.CatalogItems
+{
+  using FluentValidation;
+  
+  public class CreateCatalogItemRequestValidator : AbstractValidator<CreateCatalogItemRequest>
+  {
+
+    public CreateCatalogItemRequestValidator()
+    {
+
+      // Declare message as const here. Or could use an injected Language Service
+
+      // public const string InsufficentFundsMessage = "You can not send more than your balance";
+      // public const string MustBeGreaterThanZeroMessage = "'{PropertyName}' must be greater than zero";
+
+      RuleFor(aCreateCatalogItemRequest => aCreateCatalogItemRequest.Price)
+        .NotEmpty().GreaterThan(0);
+      RuleFor(aCreateCatalogItemRequest => aCreateCatalogItemRequest.Name)
+        .NotEmpty().MinimumLength(2);
+
+      // https://docs.fluentvalidation.net/
+      // Example Rules
+
+      // RuleFor(aSendAction => aSendAction.Fee)
+      //   .NotEmpty();
+
+      // RuleFor(aConversionRequest => aConversionRequest.FromCurrency.ToLower())
+      //   .Must(aToken => validTokens.Contains(aToken));
+
+      // RuleFor(aGetNativeAmountRequest => aGetNativeAmountRequest.ToCurrency.ToLower())
+      //   .Equal(UsdCurrencyCode.ToLower());
+
+      // RuleFor(aSendAction => aSendAction)
+      //   .Must(aSendAction => BalanceGreaterThanSendAmount(aSendAction))
+      //   .WithMessage(InsufficentFundsMessage)
+      //   .When(aSendAction => CurrencyMustExistInWallet(aSendAction))
+      //   .When(aSendAction => WalletMustExist(aSendAction.EdgeCurrencyWalletId))
+      //   .When(aSendAction => !string.IsNullOrWhiteSpace(aSendAction.NativeAmount));
+    }
+  }
+}
