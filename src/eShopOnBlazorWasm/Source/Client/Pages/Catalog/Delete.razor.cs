202f3b45df9bec67045c5779cbb6515d6d6d971c
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-13 17:14:21 +0000

    Add {Entity} to the route and as Parameter
    Add RouteFactory to make links cleaner

--- src/eShopOnBlazorWasm/Source/Client/Pages/Catalog/Delete.razor.cs
+++ src/eShopOnBlazorWasm/Source/Client/Pages/Catalog/Delete.razor.cs
@@ -3,10 +3,16 @@ namespace eShopOnBlazorWasm.Pages.Catalog
   using BlazorState.Features.Routing;
   using System.Threading.Tasks;
   using eShopOnBlazorWasm.Features.Bases;
+  using Microsoft.AspNetCore.Components;
 
   public partial class Delete: BaseComponent
   {
-    public const string Route = "/Catalog/Delete";
+    public const string Route = "/Catalog/Delete/{EntityId}";
+
+    public static string RouteFactory(int aEntityId) =>
+      Route.Replace($"{{{nameof(EntityId)}}}", aEntityId.ToString(), System.StringComparison.OrdinalIgnoreCase);
+
+    [Parameter] public string EntityId { get; set; }
 
     protected async Task ButtonClick() =>
       _ = await Mediator.Send(new RouteState.ChangeRouteAction { NewRoute = "/" });
--- src/eShopOnBlazorWasm/Source/Client/Pages/Catalog/Edit.razor.cs
+++ src/eShopOnBlazorWasm/Source/Client/Pages/Catalog/Edit.razor.cs
@@ -3,10 +3,16 @@ namespace eShopOnBlazorWasm.Pages.Catalog
   using BlazorState.Features.Routing;
   using System.Threading.Tasks;
   using eShopOnBlazorWasm.Features.Bases;
+  using Microsoft.AspNetCore.Components;
 
   public partial class Edit: BaseComponent
   {
-    public const string Route = "/Catalog/Edit";
+    public const string Route = "/Catalog/Edit/{EntityId}";
+
+    public static string RouteFactory(int aEntityId) =>
+      Route.Replace($"{{{nameof(EntityId)}}}", aEntityId.ToString(), System.StringComparison.OrdinalIgnoreCase);
+
+    [Parameter] public string EntityId { get; set; }
 
     protected async Task ButtonClick() =>
       _ = await Mediator.Send(new RouteState.ChangeRouteAction { NewRoute = "/" });
