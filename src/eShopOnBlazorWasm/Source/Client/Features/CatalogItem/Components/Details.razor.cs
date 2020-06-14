3863ec9e44078842d81d4f7c4150f8802dd1459e
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-14 04:18:57 +0000

    Start on details page

--- src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Components/Details.razor.cs
+++ src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Components/Details.razor.cs
@@ -8,6 +8,11 @@
   using System.Threading.Tasks;
   public partial class Details
   {
-    [Parameter] public string CatalogItemId { get; set; }
+    public CatalogItemDto CatalogItem => 
+      CatalogItemState.CatalogItems.SingleOrDefault(aCatalogItemDto => aCatalogItemDto.Id == CatalogItemId);
+    [Parameter] public int CatalogItemId { get; set; }
+
+    public string CatalogBrand => CatalogBrandState.CatalogBrands[CatalogItem.CatalogBrandId].Brand;
+    public string CatalogType => CatalogTypeState.CatalogTypes[CatalogItem.CatalogTypeId].Type;
   }
 }
