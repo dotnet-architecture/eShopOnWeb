a41baa4497a72395a2a762f4f3d105635f0369e0
Eric Fleming <eric-fleming18@hotmail.com>
2020-06-21 20:02:34 -0400

    Updated Unit tests after recent changes (#409)

--- tests/UnitTests/ApplicationCore/Services/BasketServiceTests/AddItemToBasket.cs
+++ tests/UnitTests/ApplicationCore/Services/BasketServiceTests/AddItemToBasket.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Services;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
 using Moq;
 using System.Threading.Tasks;
 using Xunit;
@@ -22,13 +23,13 @@ namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Services.BasketServiceTes
         {
             var basket = new Basket(_buyerId);
             basket.AddItem(1, It.IsAny<decimal>(), It.IsAny<int>());
-            _mockBasketRepo.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(basket);
+            _mockBasketRepo.Setup(x => x.FirstOrDefaultAsync(It.IsAny<BasketWithItemsSpecification>())).ReturnsAsync(basket);
 
             var basketService = new BasketService(_mockBasketRepo.Object, null);
 
             await basketService.AddItemToBasket(basket.Id, 1, 1.50m);
 
-            _mockBasketRepo.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Once);
+            _mockBasketRepo.Verify(x => x.FirstOrDefaultAsync(It.IsAny<BasketWithItemsSpecification>()), Times.Once);
         }
 
         [Fact]
@@ -36,7 +37,7 @@ namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Services.BasketServiceTes
         {
             var basket = new Basket(_buyerId);
             basket.AddItem(1, It.IsAny<decimal>(), It.IsAny<int>());
-            _mockBasketRepo.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(basket);
+            _mockBasketRepo.Setup(x => x.FirstOrDefaultAsync(It.IsAny<BasketWithItemsSpecification>())).ReturnsAsync(basket);
 
             var basketService = new BasketService(_mockBasketRepo.Object, null);
 
