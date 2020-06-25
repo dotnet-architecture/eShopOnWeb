3b1339787f63eec25e00985cfce76b1a87c5eccb
yigith <yigith1@gmail.com>
2020-06-25 03:48:23 +0300

    401 fix (#408)
    * transfer basket on login
    
    * review page
    
    * unit tests for TransferBasketAsync

---
+++ src/Web/Pages/Basket/Success.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.eShopWeb.Web.Pages.Basket
+{
+    [Authorize]
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
