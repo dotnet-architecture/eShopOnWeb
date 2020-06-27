f9d48d9bf053edab7adfe3759af1913f95d5f3e1
Steve Smith <steve@kentsmiths.com>
2020-06-26 21:32:15 -0400

    Authenticate endpoint works as bool with tests

---
+++ src/Web/API/AuthEndpoints/Authenticate.AuthenticateResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Microsoft.eShopWeb.Web.API.AuthEndpoints
+{
+    public class AuthenticateResponse : BaseResponse
+    {
+        public AuthenticateResponse(Guid correlationId) : base(correlationId)
+        {
+        }
+
+        public AuthenticateResponse()
+        {
+        }
+
+        public bool Result { get; set; }
+    }
+}
---
+++ src/Web/API/AuthEndpoints/Authenticate.cs
@@ -0,0 +1,39 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.Web.API.AuthEndpoints
+{
+    public class Authenticate : BaseAsyncEndpoint<AuthenticateRequest, AuthenticateResponse>
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public Authenticate(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("api/authenticate")]
+        [SwaggerOperation(
+            Summary = "Authenticates a user",
+            Description = "Authenticates a user",
+            OperationId = "auth.authenticate",
+            Tags = new[] { "AuthEndpoints" })
+        ]
+        public override async Task<ActionResult<AuthenticateResponse>> HandleAsync(AuthenticateRequest request)
+        {
+            var response = new AuthenticateResponse(request.CorrelationId());
+
+            var result = await _signInManager.PasswordSignInAsync(request.Username, request.Password, false, true);
+
+            response.Result = result.Succeeded;
+
+            return response;
+        }
+    }
+}
