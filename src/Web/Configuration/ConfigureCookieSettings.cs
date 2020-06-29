1939d617ec240d37c17af7184ab4e0364426fdff
Eric Fleming <eric-fleming18@hotmail.com>
2020-06-29 14:00:54 -0400

    Refactor startup (#412)
    * Removing unused folder
    
    * Refactoring Startup.cs

---
+++ src/Web/Configuration/ConfigureCookieSettings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Microsoft.eShopWeb.Web.Configuration
+{
+    public static class ConfigureCookieSettings
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromHours(1);
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+                options.Cookie = new CookieBuilder
+                {
+                    IsEssential = true // required for auth to work without explicit user consent; adjust to suit your privacy policy
+                };
+            });
+        }
+    }
+}
