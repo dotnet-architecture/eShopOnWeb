74cd2b2a90f3491a4c8e226c4254e56137b13a77
Eric Fleming <eric-fleming18@hotmail.com>
2020-06-29 21:01:28 -0400

    Uncomment and update ApiCatalogControllerLists tests

--- tests/FunctionalTests/PublicApi/CatalogItemEndpoints/ApiCatalogControllerList.cs
+++ tests/FunctionalTests/PublicApi/CatalogItemEndpoints/ApiCatalogControllerList.cs
@@ -1,44 +1,42 @@
-﻿using Microsoft.eShopWeb.Web.ViewModels;
+﻿using Microsoft.eShopWeb.FunctionalTests.PublicApi;
+using Microsoft.eShopWeb.Web.ViewModels;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Microsoft.eShopWeb.FunctionalTests.Web.Controllers
 {
-    //[Collection("Sequential")]
-    //public class ApiCatalogControllerList : IClassFixture<ApiTestFixture>
-    //{
-    //    JsonSerializerOptions _jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    [Collection("Sequential")]
+    public class ApiCatalogControllerList : IClassFixture<ApiTestFixture>
+    {
+        public ApiCatalogControllerList(ApiTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
 
-    //    public ApiCatalogControllerList(ApiTestFixture factory)
-    //    {
-    //        Client = factory.CreateClient();
-    //    }
+        public HttpClient Client { get; }
 
-    //    public HttpClient Client { get; }
+        [Fact]
+        public async Task ReturnsFirst10CatalogItems()
+        {
+            var response = await Client.GetAsync("/api/catalog-items?pageSize=10");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var model = stringResponse.FromJson<CatalogIndexViewModel>();
 
-    //    [Fact]
-    //    public async Task ReturnsFirst10CatalogItems()
-    //    {
-    //        var response = await Client.GetAsync("/api/catalog/list");
-    //        response.EnsureSuccessStatusCode();
-    //        var stringResponse = await response.Content.ReadAsStringAsync();
-    //        var model = stringResponse.FromJson<CatalogIndexViewModel>();
+            Assert.Equal(10, model.CatalogItems.Count());
+        }
 
-    //        Assert.Equal(10, model.CatalogItems.Count());
-    //    }
+        [Fact]
+        public async Task ReturnsLast2CatalogItemsGivenPageIndex1()
+        {
+            var response = await Client.GetAsync("/api/catalog-items?pageSize=10&pageIndex=1");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var model = stringResponse.FromJson<CatalogIndexViewModel>();
 
-    //    [Fact]
-    //    public async Task ReturnsLast2CatalogItemsGivenPageIndex1()
-    //    {
-    //        var response = await Client.GetAsync("/api/catalog/list?page=1");
-    //        response.EnsureSuccessStatusCode();
-    //        var stringResponse = await response.Content.ReadAsStringAsync();
-    //        var model = stringResponse.FromJson<CatalogIndexViewModel>();
-
-    //        Assert.Equal(2, model.CatalogItems.Count());
-    //    }
-    //}
+            Assert.Equal(2, model.CatalogItems.Count());
+        }
+    }
 }
