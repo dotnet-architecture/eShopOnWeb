89c41d3739131650755ab3485cbcdb380789d047
Steve Smith <steve@kentsmiths.com>
2020-07-03 12:46:33 -0400

    Updated CatalogItem to support more updates; added tests

--- src/PublicApi/CatalogItemEndpoints/Update.UpdateCatalogItemRequest.cs
+++ src/PublicApi/CatalogItemEndpoints/Update.UpdateCatalogItemRequest.cs
@@ -1,14 +1,21 @@
-﻿namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints
 {
     public class UpdateCatalogItemRequest : BaseRequest
     {
+        [Range(1, 10000)]
         public int Id { get; set; }
+        [Range(1, 10000)]
         public int CatalogBrandId { get; set; }
+        [Range(1, 10000)]
         public int CatalogTypeId { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Name { get; set; }
         public string PictureUri { get; set; }
+        [Range(0.01, 10000)]
         public decimal Price { get; set; }
     }
-
 }
