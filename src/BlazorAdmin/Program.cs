ed80324f9d12ff21baef540ddfe3255b101eaf09
Shady Nagy <shadynagi@gmail.com>
2020-07-25 01:08:11 +0200

    SecureHttpClient removed

--- src/BlazorAdmin/Program.cs
+++ src/BlazorAdmin/Program.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using BlazorAdmin.Network;
 using BlazorAdmin.Services;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -18,7 +17,6 @@ namespace BlazorAdmin
             builder.RootComponents.Add<App>("admin");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddTransient(sp => new SecureHttpClient(sp.GetRequiredService<HttpClient>()));
 
             builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
             builder.Services.AddSingleton<AuthService>();
