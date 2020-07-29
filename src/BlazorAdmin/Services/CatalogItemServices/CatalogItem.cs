48ecaddb62a62a29b372ff946bfa16ec6f067bc2
Shady Nagy <shadynagi@gmail.com>
2020-07-29 02:11:54 +0200

    Fixed Edit for remove image and keep image and change other fields. Added description required in Blazor Admin.

--- src/BlazorAdmin/Services/CatalogItemServices/CatalogItem.cs
+++ src/BlazorAdmin/Services/CatalogItemServices/CatalogItem.cs
@@ -17,6 +17,7 @@ namespace BlazorAdmin.Services.CatalogItemServices
         [Required(ErrorMessage = "The Name field is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "The Description field is required")]
         public string Description { get; set; }
 
         // decimal(18,2)
--- src/BlazorAdmin/Services/CatalogItemServices/Create.CreateCatalogItemRequest.cs
+++ src/BlazorAdmin/Services/CatalogItemServices/Create.CreateCatalogItemRequest.cs
@@ -11,6 +11,7 @@ namespace BlazorAdmin.Services.CatalogItemServices
         [Required(ErrorMessage = "The Name field is required")]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "The Description field is required")]
         public string Description { get; set; } = string.Empty;
 
         // decimal(18,2)
