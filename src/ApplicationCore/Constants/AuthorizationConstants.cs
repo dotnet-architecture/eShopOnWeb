1e1f4e602050663a118b9e8d7af50904bd55f1e1
Andy Sterland <andster@microsoft.com>
2020-09-03 23:19:32 +0000

    fixing issue with db migration

--- src/ApplicationCore/Constants/AuthorizationConstants.cs
+++ src/ApplicationCore/Constants/AuthorizationConstants.cs
@@ -7,6 +7,12 @@
             public const string ADMINISTRATORS = "Administrators";
         }
 
+        public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
+
+        // TODO: Don't use this in production
         public const string DEFAULT_PASSWORD = "Pass@word1";
+
+        // TODO: Change this to an environment variable
+        public const string JWT_SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
     }
 }
