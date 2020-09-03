0d8f0213089f19ee0bccbf9c1bb9f25dc3bf3abf
Andy Sterland <andster@microsoft.com>
2020-09-03 15:46:43 -0700

    Intial commit for customizations

---
+++ postCloneSetup.ps1
@@ -0,0 +1,3 @@
+devinit init
+dotnet ef database update -c catalogcontext -p src\Infrastructure\Infrastructure.csproj -s src\Web\Web.csproj
+dotnet ef database update -c appidentitydbcontext -p src\Infrastructure\Infrastructure.csproj -s src\Web\Web.csproj
