76eadcddccdf816fed8b17a3c03183ed2bb60e46
Sumit Ghosh <13281246+sughosneo@users.noreply.github.com>
2020-12-02 16:56:12 +0530

    Added net5 migration plans

--- src/Infrastructure/Identity/Migrations/20201202111612_InitialIdentityModel.Designer.cs
+++ src/Infrastructure/Identity/Migrations/20201202111612_InitialIdentityModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.eShopWeb.Infrastructure.Identity;
 
 namespace Microsoft.eShopWeb.Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201202111612_InitialIdentityModel")]
+    partial class InitialIdentityModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
