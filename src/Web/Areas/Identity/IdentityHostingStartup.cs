54defbe963c8fe2f58f7f08c3444e68cd58bf1a1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-03 18:35:28 +0530

    Swamy/remove unused usings and reorganize usings (#489)
    * Removed and Reordered the using statements
    
    * Removed and Reordered the usings inside Web Project
    
    * Removed and Reordered the usings inside PublicApi project
    
    * Removed Unused usings and reorganized usings inside Infrastructure project

--- src/Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Web/Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.eShopWeb.Infrastructure.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(Microsoft.eShopWeb.Web.Areas.Identity.IdentityHostingStartup))]
 namespace Microsoft.eShopWeb.Web.Areas.Identity
--- src/Web/ViewModels/File/FileViewModel.cs
+++ src/Web/ViewModels/File/FileViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Microsoft.eShopWeb.Web.ViewModels.File
+﻿namespace Microsoft.eShopWeb.Web.ViewModels.File
 {
     public class FileViewModel
     {
