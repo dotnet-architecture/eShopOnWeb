ae036a9794d15432c0533a2b5d2703cdf84f9768
Sumit Ghosh <13281246+sughosneo@users.noreply.github.com>
2021-06-29 11:53:43 +0530

    Included feedback

--- src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -7,11 +7,11 @@ namespace Microsoft.eShopWeb.ApplicationCore.Specifications
     {
         public CatalogFilterPaginatedSpecification(int skip, int take, int? brandId, int? typeId)
             : base()
-        {
+        {                           
             Query
                 .Where(i => (!brandId.HasValue || i.CatalogBrandId == brandId) &&
                 (!typeId.HasValue || i.CatalogTypeId == typeId))
-                .Paginate(skip, take);
+                .Skip(skip).Take(take);
         }
     }
 }
