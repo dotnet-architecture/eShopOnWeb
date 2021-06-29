89e9fd531bdfcded1ed3e17bd09d55d674102d78
Taras Laba <taras.laba@dat.com>
2021-06-29 13:22:48 +0300

    fix startup

--- src/PublicApi/Startup.cs
+++ src/PublicApi/Startup.cs
@@ -17,7 +17,6 @@ using Microsoft.eShopWeb.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Collections.Generic;
