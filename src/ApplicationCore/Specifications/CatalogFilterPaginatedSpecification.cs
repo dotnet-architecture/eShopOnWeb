5b5abd9c8dc9ddfb737a2c9b3fa419e4d1dfc304
Cédric Michel <cedric.michel@outlook.be>
2021-10-24 23:41:36 +0200

    Issue/567 (#568)
    * Correctly manage page count when Page size = 0
    
    * make =>TODO: Need to change the api to support full list
    Manage to support full list when pagesize is unknow

--- src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -7,7 +7,11 @@ namespace Microsoft.eShopWeb.ApplicationCore.Specifications
     {
         public CatalogFilterPaginatedSpecification(int skip, int take, int? brandId, int? typeId)
             : base()
-        {                           
+        {
+            if (take == 0)
+            {
+                take = int.MaxValue;
+            }
             Query
                 .Where(i => (!brandId.HasValue || i.CatalogBrandId == brandId) &&
                 (!typeId.HasValue || i.CatalogTypeId == typeId))
