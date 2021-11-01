5d34222f28e14958f3e6e12b44fc88f85366b310
Cédric Michel <cedric.michel@outlook.be>
2021-11-01 14:49:53 +0100

    add cached on logout with revoke cookie identity key (#605)
    * add cached on logout with revoke cookie identity key
    
    * properly signout as recommended : https://docs.microsoft.com/en-us/aspnet/core/security/authentication/cookie?view=aspnetcore-5.0#react-to-back-end-changes
    
    * add remark regarding multi-host scenario
    
    * Update src/Web/Configuration/RevokeAuthenticationEvents.cs
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- src/Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,25 +1,31 @@
-﻿using System.Threading.Tasks;
-using AutoMapper.Configuration.Annotations;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.eShopWeb.Infrastructure.Identity;
+using Microsoft.eShopWeb.Web.Configuration;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Web.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    [IgnoreAntiforgeryToken]
+    //TODO : replace IMemoryCache by distributed cache if you are in multi-host scenario
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IMemoryCache _cache;
 
-        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger, IMemoryCache cache)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _cache = cache;
         }
 
         public void OnGet()
@@ -29,6 +35,14 @@ namespace Microsoft.eShopWeb.Web.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            var userId = _signInManager.Context.User.Claims.First(c => c.Type == ClaimTypes.Name);
+            var identityKey = _signInManager.Context.Request.Cookies[ConfigureCookieSettings.IdentifierCookieName];
+            _cache.Set($"{userId.Value}:{identityKey}", identityKey, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(ConfigureCookieSettings.ValidityMinutesPeriod)
+            });
+
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
