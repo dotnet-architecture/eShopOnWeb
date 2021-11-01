ed30f3dcc4633e756febac0a5f65d185df624ea0
Cédric Michel <cedric.michel@outlook.be>
2021-11-01 15:35:26 +0100

    fix bad redirection (#608)
    * fix bad redirection
    fix cleanning of local storage on startup
    
    * update all nuget package
    
    * avoid magic string

--- src/BlazorAdmin/Program.cs
+++ src/BlazorAdmin/Program.cs
@@ -1,6 +1,7 @@
 using BlazorAdmin.Services;
 using Blazored.LocalStorage;
 using BlazorShared;
+using BlazorShared.Models;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -27,13 +28,13 @@ namespace BlazorAdmin
 
             builder.Services.AddScoped<ToastService>();
             builder.Services.AddScoped<HttpService>();
-           
+
             builder.Services.AddBlazoredLocalStorage();
-           
+
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
             builder.Services.AddScoped(sp => (CustomAuthStateProvider)sp.GetRequiredService<AuthenticationStateProvider>());
-           
+
             builder.Services.AddBlazorServices();
 
             builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
@@ -48,7 +49,8 @@ namespace BlazorAdmin
             var sp = services.BuildServiceProvider();
             var localStorageService = sp.GetRequiredService<ILocalStorageService>();
 
-            await localStorageService.RemoveItemAsync("brands");
+            await localStorageService.RemoveItemAsync(typeof(CatalogBrand).Name);
+            await localStorageService.RemoveItemAsync(typeof(CatalogType).Name);
         }
     }
 }
