64f150dc0798d3f9334b7b72a0e71b5f0935f1e3
Cédric Michel <cedric.michel@outlook.be>
2021-11-02 15:16:57 +0100

    replace counter by specific query to avoid load each items in memory (#611)
    * replace counter by specific query to avoid load each items in memory
    
    * Create IBasketQueryService

---
+++ src/ApplicationCore/Interfaces/IBasketQueryService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Interfaces
+{
+    public interface IBasketQueryService
+    {
+        Task<int> CountTotalBasketItems(string username);
+    }
+}
--- src/Web/Configuration/ConfigureCoreServices.cs
+++ src/Web/Configuration/ConfigureCoreServices.cs
@@ -13,13 +13,12 @@ namespace Microsoft.eShopWeb.Web.Configuration
         public static IServiceCollection AddCoreServices(this IServiceCollection services,
             IConfiguration configuration)
         {
-            //services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
             services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
             services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
 
             services.AddScoped<IBasketService, BasketService>();
             services.AddScoped<IOrderService, OrderService>();
-            //services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IBasketQueryService, BasketQueryService>();
             services.AddSingleton<IUriComposer>(new UriComposer(configuration.Get<CatalogSettings>()));
             services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
             services.AddTransient<IEmailSender, EmailSender>();
