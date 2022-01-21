6f640906e9f203d52c080c8ffac05008e778c57b
Cedric <cedric.michel@outlook.be>
2022-01-21 09:20:40 +0100

    fix pay now

--- src/Web/Pages/Basket/BasketItemViewModel.cs
+++ src/Web/Pages/Basket/BasketItemViewModel.cs
@@ -6,12 +6,12 @@ public class BasketItemViewModel
 {
     public int Id { get; set; }
     public int CatalogItemId { get; set; }
-    public string ProductName { get; set; }
+    public string? ProductName { get; set; }
     public decimal UnitPrice { get; set; }
     public decimal OldUnitPrice { get; set; }
 
     [Range(0, int.MaxValue, ErrorMessage = "Quantity must be bigger than 0")]
     public int Quantity { get; set; }
 
-    public string PictureUrl { get; set; }
+    public string? PictureUrl { get; set; }
 }
