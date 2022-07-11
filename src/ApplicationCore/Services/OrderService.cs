8cbd9dacc859c8d06946a928e662501c318e9beb
Gary <pgciq@126.com>
2022-07-11 17:56:52 +0800

    [EP-2022Q2] upload order to blob

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
@@ -6,6 +7,8 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -15,16 +18,20 @@ public class OrderService : IOrderService
     private readonly IUriComposer _uriComposer;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly IAppLogger<OrderService> _logger;
+    private readonly string _funcUrl = "https://orderitem0711.azurewebsites.net/api/OrderItems?";
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        IAppLogger<OrderService> logger)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
+        _logger = logger;
     }
 
     public async Task CreateOrderAsync(int basketId, Address shippingAddress)
@@ -49,5 +56,12 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+
+        _logger.LogInformation("start sending http request");
+        HttpClient client = new HttpClient();
+        string orderJson = JsonConvert.SerializeObject(order);
+        string parameter = $"orderDetail={orderJson}";
+        await client.GetAsync(_funcUrl + parameter);
+        _logger.LogInformation("sending http request end");
     }
 }
