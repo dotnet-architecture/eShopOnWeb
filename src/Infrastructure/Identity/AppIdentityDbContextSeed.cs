6bec771953d63c2f91ccdb6d8f87eb8564842ede
Anatolii Gutak <agutak@outlook.com>
2022-07-25 01:31:46 +0300

    Merge upstream

--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -7,13 +7,13 @@ namespace Microsoft.eShopWeb.Infrastructure.Identity;
 
 public class AppIdentityDbContextSeed
 {
-    public static async Task SeedAsync(
-        AppIdentityDbContext context,
-        UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager)
+    public static async Task SeedAsync(AppIdentityDbContext identityDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
     {
-        if (context.Database.IsSqlServer())
-            context.Database.Migrate();
+
+        if (identityDbContext.Database.IsSqlServer())
+        {
+            identityDbContext.Database.Migrate();
+        }
 
         await roleManager.CreateAsync(new IdentityRole(BlazorShared.Authorization.Constants.Roles.ADMINISTRATORS));
 
