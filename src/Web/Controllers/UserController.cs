bb406e5d974dd9fbbe25054295322a3437e0258e
cedri <cedri@BAS>
2022-12-22 11:37:06 +0100

    fix redirect to login for admin page fix logout

--- src/Web/Controllers/UserController.cs
+++ src/Web/Controllers/UserController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
 using BlazorShared.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+using Microsoft.eShopWeb.Web.Configuration;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Microsoft.eShopWeb.Web.Controllers;
 
@@ -14,10 +17,19 @@ namespace Microsoft.eShopWeb.Web.Controllers;
 public class UserController : ControllerBase
 {
     private readonly ITokenClaimsService _tokenClaimsService;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly ILogger<UserController> _logger;
+    private readonly IMemoryCache _cache;
 
-    public UserController(ITokenClaimsService tokenClaimsService)
+    public UserController(ITokenClaimsService tokenClaimsService,
+                          SignInManager<ApplicationUser> signInManager,
+                          ILogger<UserController> logger,
+                          IMemoryCache cache)
     {
         _tokenClaimsService = tokenClaimsService;
+        _signInManager = signInManager;
+        _logger = logger;
+        _cache = cache;
     }
 
     [HttpGet]
@@ -25,6 +37,25 @@ public class UserController : ControllerBase
     [AllowAnonymous]
     public async Task<IActionResult> GetCurrentUser() =>
         Ok(await CreateUserInfo(User));
+
+    [Route("Logout")]
+    [HttpPost]
+    [Authorize]
+    [AllowAnonymous]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var userId = _signInManager.Context.User.Claims.First(c => c.Type == ClaimTypes.Name);
+        var identityKey = _signInManager.Context.Request.Cookies[ConfigureCookieSettings.IdentifierCookieName];
+        _cache.Set($"{userId.Value}:{identityKey}", identityKey, new MemoryCacheEntryOptions
+        {
+            AbsoluteExpiration = DateTime.Now.AddMinutes(ConfigureCookieSettings.ValidityMinutesPeriod)
+        });
+
+        _logger.LogInformation("User logged out.");
+        return Ok();
+    }
 
     private async Task<UserInfo> CreateUserInfo(ClaimsPrincipal claimsPrincipal)
     {
