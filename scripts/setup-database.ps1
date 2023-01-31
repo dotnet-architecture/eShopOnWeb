fc3f7b27469c1370cc9a14f99c3aa37c4eaf33a3
zedy <zedy@wicresoft.com>
2023-01-31 10:50:21 +0800

    Remove invalid files

--- scripts/setup-database.ps1
+++
@@ -1,9 +0,0 @@
-# Default to current directory
-$FOLDER_PATH = [string](Get-Location)
-
-# Before setting up your database, make sure of two things:
-# 1. Ensure your connection strings in appsettings.json point to a local SQL Server instance.
-# 2. Ensure the tool EF was already installed.
-dotnet tool update --global dotnet-ef
-dotnet ef database update -c catalogcontext -p $FOLDER_PATH/src/Infrastructure/Infrastructure.csproj -s $FOLDER_PATH/src/Web/Web.csproj
-dotnet ef database update -c appidentitydbcontext  -p $FOLDER_PATH/src/Infrastructure/Infrastructure.csproj -s $FOLDER_PATH/src/Web/Web.csproj
