ef333e5a08d169edecf52be04cddf8df5c8af5e3
Maria Adzinets <Maria_Adzinets@epam.com>
2023-08-14 11:20:59 +0200

    add func

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
@@ -6,6 +8,7 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Newtonsoft.Json;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -15,16 +18,21 @@ public class OrderService : IOrderService
     private readonly IUriComposer _uriComposer;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly HttpClient _httpClient;
+
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        HttpClient httpClient)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
+        _httpClient = httpClient;
+
     }
 
     public async Task CreateOrderAsync(int basketId, Address shippingAddress)
@@ -49,5 +57,33 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+
+        await SendOrderToFunction(order);
+    }
+    private async Task SendOrderToFunction(Order order)
+    {
+        try
+        {
+            string functionUrl = "https://orderreserverservice20230802222629.azurewebsites.net/api/OrderItemsReserver";
+
+            var itemReservation = new
+            {
+                orderId = order.Id,
+                orderItems = order.OrderItems.Select(orderItem => new
+                {
+                    itemId = orderItem.ItemOrdered.CatalogItemId,
+                    quantity = orderItem.Units
+                }).ToList()
+            };
+            string jsonData = JsonConvert.SerializeObject(itemReservation);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await _httpClient.PostAsync(functionUrl, content);            
+        }
+        catch
+        {
+            throw;
+        }
+       
     }
 }
