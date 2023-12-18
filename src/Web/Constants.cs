fb088dd953af846cd8fff5efe44495d7fffe0cbc
kadiyalabhargavi <92821350+kadiyalabhargavi@users.noreply.github.com>
2023-12-18 09:40:34 -0700

    Update Constants.cs

--- src/Web/Constants.cs
+++ src/Web/Constants.cs
@@ -1,9 +1,11 @@
 ﻿namespace Microsoft.eShopWeb.Web;
 
+
 public static class Constants
 {
     public const string BASKET_COOKIENAME = "eShop";
     public const int ITEMS_PER_PAGE = 10;
     public const string DEFAULT_USERNAME = "Guest";
     public const string BASKET_ID = "BasketId";
+    public const string BASKET_NAME = "eShopnonWebApp";
 }
