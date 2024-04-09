063338f012081365d20d72018ba463a278422b90
yusufali0 <yusufali.bekci@hotmail.com>
2024-04-09 22:05:20 +0300

    v.1

--- src/Web/ViewModels/OrderDetailViewModel.cs
+++ src/Web/ViewModels/OrderDetailViewModel.cs
@@ -2,5 +2,8 @@
 
 public class OrderDetailViewModel : OrderViewModel
 {
+    public int UserId { get; set; }
     public List<OrderItemViewModel> OrderItems { get; set; } = new();
+    public bool Shipped { get; set; }
+    public int OrderId { get; set; }
 }
